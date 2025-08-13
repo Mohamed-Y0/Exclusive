f7457f3227003347bcc6f7bcaa59963373e7d37e
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-13 06:57:08 +0300

    Initial Type For Products

---
+++ src/types/products.ts
@@ -0,0 +1,46 @@
+export type ProductApiResponse = {
+  products: ProductTypes[];
+  total: number;
+  skip: number;
+  limit: number;
+};
+
+export type ProductTypes = {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: {
+    width: number;
+    height: number;
+    depth: number;
+  };
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: {
+    rating: number;
+    comment: string;
+    date: string; // ISO
+    reviewerName: string;
+    reviewerEmail: string;
+  }[];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: {
+    createdAt: string; // ISO
+    updatedAt: string; // ISO
+    barcode: string;
+    qrCode: string;
+  };
+  thumbnail: string;
+  images: string[];
+};
