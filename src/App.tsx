58aad3b41e5758cf8b4cf48c3d4c440fba25d0fc
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-14 01:42:15 +0300

    init - react router with home page and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import AppLayout from "@/components/layout/AppLayout";
+import Home from "@/pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <p>Something Wrong</p>,
+    children: [{ path: "/", element: <Home /> }],
+  },
+]);
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
