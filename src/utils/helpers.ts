dc38747b0d3ffb6509917088071f821a6aaf8684
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-18 02:05:57 +0300

    Helpers - formatCurrency, generateRandomIds

---
+++ src/utils/helpers.ts
@@ -0,0 +1,19 @@
+export function formatCurrency(value: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+}
+
+export function generateRandomIds(count: number): number[] {
+  const ids: number[] = [];
+
+  while (ids.length < count) {
+    const randomId = Math.floor(Math.random() * 194) + 1; // من 1 لـ 194
+    if (!ids.includes(randomId)) {
+      ids.push(randomId);
+    }
+  }
+
+  return ids;
+}
