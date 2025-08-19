2982a8c0db04623062af88295864874de1fba780
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-19 16:57:03 +0300

    Finish - flash sales

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -4,7 +4,7 @@ import LeftMenu from "@/components/home/LeftMenu";
 
 function HeroSection() {
   return (
-    <section className="py-5 sm:py-16">
+    <section className="border-b-2 border-neutral-200 py-5 sm:py-16">
       <div className="grid grid-cols-1 pb-10 lg:grid-cols-5">
         <LeftMenu />
         <ImageSlider />
