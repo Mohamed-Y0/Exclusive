22f356d843f0db57cddeee5625fb8ad219685220
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-19 17:32:59 +0300

    fix - padding

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -5,7 +5,7 @@ import LeftMenu from "@/components/home/LeftMenu";
 function HeroSection() {
   return (
     <section className="border-b-2 border-neutral-200 py-5 sm:py-16">
-      <div className="grid grid-cols-1 pb-10 lg:grid-cols-5">
+      <div className="grid grid-cols-1 gap-5 pb-10 lg:grid-cols-5 lg:gap-0">
         <LeftMenu />
         <ImageSlider />
       </div>
