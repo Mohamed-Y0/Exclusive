a7d4afb3263a2a2a525602bacecd9c43615f9477
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-28 21:15:44 +0300

    make Input.tsx more re-usable

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,13 +1,14 @@
 type InputProps = {
   label?: string;
+  hidden?: boolean;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-function Input({ label, ...props }: InputProps) {
+function Input({ label, hidden, ...props }: InputProps) {
   return (
-    <div>
+    <div className={`${hidden && "hidden sm:block"}`}>
       {label && <label>{label}</label>}
       <input
-        className="w-full rounded-lg bg-white px-2.5 py-1 text-black focus:outline-1 focus:outline-neutral-200 lg:px-5 lg:py-2.5"
+        className={`w-full rounded-lg bg-white px-2.5 py-1 text-black focus:outline-1 focus:outline-neutral-200 lg:px-5 lg:py-2.5`}
         {...props}
       />
     </div>
