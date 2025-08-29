2760b40adbdd4e32c26e5c56cf1c490f877060fa
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-29 18:03:51 +0300

    ref: make it more re-usable

--- src/components/home/LeftMenu.tsx
+++ src/components/home/LeftMenu.tsx
@@ -41,14 +41,18 @@ const categories = [
   },
 ];
 
-function LeftMenu() {
+type Props = {
+  hide?: boolean;
+};
+
+function LeftMenu({ hide }: Props) {
   return (
-    <div className="">
-      <ul className="md:flexwr hidden px-5 sm:flex sm:flex-wrap sm:justify-center lg:flex-col lg:border-r lg:border-neutral-200 lg:px-0 lg:pr-2.5">
+    <div className={`md:block ${hide && "hidden"} `}>
+      <ul className="sm:flex-wrap sm:justify-center sm:px-5 md:flex lg:flex-col lg:border-r lg:border-neutral-200 lg:px-0 lg:pr-2.5">
         {categories.map((item) => (
           <li
             key={item.title}
-            className="px-5 py-2.5 hover:bg-neutral-100 lg:pr-2.5"
+            className="rounded-sm px-2.5 py-2.5 duration-300 hover:bg-neutral-100 sm:px-5 lg:pr-2.5"
           >
             <Link to="/">{item.title}</Link>
           </li>
