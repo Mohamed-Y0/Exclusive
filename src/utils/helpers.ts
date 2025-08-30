b5550f25d061fe417f5c88ce1b227ff16e27d025
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-30 21:13:39 +0300

    add: capitalizeFirstLetter function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -23,3 +23,8 @@ export function getWindowSize() {
 
   return windowSize;
 }
+
+export function capitalizeFirstLetter(str: string): string {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
