91fe5bbd4e35f75572b8d40bd028956e4989011f
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-30 03:00:33 +0300

    add: id, htmlFor for Input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,13 +1,15 @@
 type InputProps = {
   label?: string;
   hidden?: boolean;
+  id?: string;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-function Input({ label, hidden, ...props }: InputProps) {
+function Input({ label, hidden, id, ...props }: InputProps) {
   return (
     <div className={`${hidden && "hidden sm:block"}`}>
-      {label && <label>{label}</label>}
+      {label && <label htmlFor={id}>{label}</label>}
       <input
+        id={id}
         className={`w-full rounded-lg bg-white px-2.5 py-1 text-black focus:outline-1 focus:outline-neutral-200 lg:px-5 lg:py-2.5`}
         {...props}
       />
