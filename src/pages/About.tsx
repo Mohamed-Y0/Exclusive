7fc84739e626a3d5e7ecb82ace6afb118b642ec5
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-08-31 04:19:52 +0300

    bruh

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
 import AboutHero from "@/components/about/AboutHero";
 import Statistics from "@/components/about/Statistics";
+import StoreBenefits from "@/components/home/StoreBenefits";
 import PageLocation from "@/components/ui/PageLocation";
 
 function About() {
@@ -8,6 +9,7 @@ function About() {
       <PageLocation />
       <AboutHero />
       <Statistics />
+      <StoreBenefits />
     </div>
   );
 }
