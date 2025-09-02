8107b87d7e809d7a3bc618abf0ae50fa745ba220
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-03 02:38:15 +0300

    add: init style

--- src/pages/CategoryProducts.tsx
+++ src/pages/CategoryProducts.tsx
@@ -2,7 +2,7 @@ import Category from "@/components/category/DisplayCategory";
 
 function CategoryProducts() {
   return (
-    <div>
+    <div className="container m-auto p-5 lg:p-0">
       <Category />
     </div>
   );
