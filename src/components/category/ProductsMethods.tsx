c38049c707d3f833fc85da4b2444725ddbc1c433
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-03 03:48:42 +0300

    init methods

--- src/components/category/ProductsMethods.tsx
+++ src/components/category/ProductsMethods.tsx
@@ -1,5 +1,11 @@
-function Filter() {
-  return <div></div>;
+function ProductsMethods() {
+  return (
+    <div className="pt-1 font-light">
+      <div className="h-fit rounded-sm p-2.5 shadow shadow-neutral-300">
+        <p>Methods</p>
+      </div>
+    </div>
+  );
 }
 
-export default Filter;
+export default ProductsMethods;
