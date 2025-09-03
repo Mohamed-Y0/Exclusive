4505d451fc10c93d6164e8d53ea89144dfdd664a
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-03 03:49:37 +0300

    add the component and display them

--- src/pages/CategoryProducts.tsx
+++ src/pages/CategoryProducts.tsx
@@ -1,10 +1,18 @@
 import Category from "@/components/category/DisplayCategory";
+import ProductsMethods from "@/components/category/ProductsMethods";
+import PageLocation from "@/components/ui/PageLocation";
 
 function CategoryProducts() {
   return (
-    <div className="container m-auto p-5 lg:p-0">
-      <Category />
-    </div>
+    <>
+      <div className="container m-auto p-5 lg:p-0">
+        <PageLocation />
+        <div className="grid grid-cols-5 gap-5 py-16">
+          <ProductsMethods />
+          <Category />
+        </div>
+      </div>
+    </>
   );
 }
 
