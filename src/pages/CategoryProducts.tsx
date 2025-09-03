249e03e3316eeef5b938439d75a317c6d02ba4c6
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-04 02:08:41 +0300

    refactor

--- src/pages/CategoryProducts.tsx
+++ src/pages/CategoryProducts.tsx
@@ -1,18 +1,12 @@
 import Category from "@/components/category/DisplayCategory";
-import ProductsMethods from "@/components/category/ProductsMethods";
 import PageLocation from "@/components/ui/PageLocation";
 
 function CategoryProducts() {
   return (
-    <>
-      <div className="container m-auto lg:p-0">
-        <PageLocation />
-        <div className="grid grid-cols-1 justify-between gap-5 py-10 sm:grid-cols-3 lg:grid-cols-5 lg:py-16">
-          <Category />
-          <ProductsMethods />
-        </div>
-      </div>
-    </>
+    <div className="container m-auto lg:p-0">
+      <PageLocation />
+      <Category />
+    </div>
   );
 }
 
