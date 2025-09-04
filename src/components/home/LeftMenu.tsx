67f37afbe654e94bc1ae589995897b60faeffa2d
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-04 15:46:13 +0300

    edit names

--- src/components/home/LeftMenu.tsx
+++ src/components/home/LeftMenu.tsx
@@ -3,33 +3,24 @@ import { Link, NavLink } from "react-router-dom";
 
 const categories = [
   {
-    title: "mens",
-    items: ["Shirts", "Shoes", "Watches"],
+    name: "mens",
+    items: ["shirts", "shoes", "watches", "sunglasses"],
   },
   {
-    title: "womens",
-    items: [" Bags", " Dresses", " Jewellery", " Shoes", " Watches"],
+    name: "womens",
+    items: ["dresses", "bags", "shoes", "watches", "jewellery", "tops"],
+  },
+  { name: "beauty", items: ["beauty", "skin-care", "fragrances"] },
+  {
+    name: "electronics",
+    items: ["smartphones", "laptops", "tablets", "mobile-accessories"],
   },
   {
-    title: "electronics",
-    items: ["smartphones", "tablets", "laptops", "Accessories"],
+    name: "home",
+    items: ["furniture", "home-decoration", "kitchen-accessories", "groceries"],
   },
-  {
-    title: "home",
-    items: ["furniture", "decoration", "kitchen", "groceries"],
-  },
-  {
-    title: "beauty",
-    items: ["skin care", "beauty", "fragrances"],
-  },
-  {
-    title: "sports",
-    items: ["motorcycle"],
-  },
-  {
-    title: "motors",
-    items: ["motorcycle", "vehicle"],
-  },
+  { name: "sports", items: ["sports-accessories"] },
+  { name: "motors", items: ["motorcycle", "vehicle"] },
 ];
 
 type Props = {
@@ -42,11 +33,11 @@ function LeftMenu({ hide }: Props) {
       <ul className="sm:flex-wrap sm:justify-center sm:px-5 md:flex lg:flex-col lg:border-r lg:border-neutral-200 lg:px-0">
         {categories.map((item) => (
           <li
-            key={item.title}
+            key={item.name}
             className="group relative rounded-sm px-2.5 py-2.5 duration-300 hover:bg-neutral-100 sm:px-5 lg:pr-2.5"
           >
             <div className="flex items-center justify-between">
-              <Link to={`/products/${item.title}`}>{item.title}</Link>
+              <Link to={`/products/${item.name}`}>{item.name}</Link>
               {item.items.length > 1 && (
                 <MdOutlineKeyboardArrowRight className="" />
               )}
@@ -56,7 +47,7 @@ function LeftMenu({ hide }: Props) {
               {item.items.length > 1 &&
                 item.items.map((category) => (
                   <NavLink
-                    to={`/products/${item.title}/${category}`}
+                    to={`/products/${item.name}/${category}`}
                     key={category}
                   >
                     <li className="hidden px-2.5 py-2.5 group-hover:block hover:text-neutral-500 sm:px-5 lg:pr-2.5">
