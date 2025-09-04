b87ff9c6927ed35fe26c5df593c88dd9026c525f
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-05 02:17:25 +0300

    add logo

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -4,7 +4,14 @@ type LogoProps = {
 
 function Logo({ color }: LogoProps) {
   return (
-    <p className={`text-lg font-bold tracking-wider text-${color} lg:text-2xl`}>
+    <p
+      className={`text-lg font-bold tracking-wider text-${color} flex items-center gap-2.5 lg:text-2xl`}
+    >
+      <img
+        src={`/imgs/logo-${color}-70.webp`}
+        className="h-full w-full"
+        alt=""
+      />
       Exclusive
     </p>
   );
