d82ead3d0567741a39f0671a3f528747d3a6914a
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-05 02:26:40 +0300

    improve styles

--- src/components/home/LeftMenu.tsx
+++ src/components/home/LeftMenu.tsx
@@ -28,11 +28,11 @@ type Props = {
 
 function LeftMenu({ hide }: Props) {
   return (
-    <div className={`md:block ${hide && "hidden"} capitalize`}>
-      <ul className="sm:flex-wrap sm:justify-center md:flex lg:flex-col lg:border-r lg:border-neutral-200">
+    <div className={`sm:block ${hide && "hidden"} capitalize`}>
+      <ul className="sm:flex sm:flex-wrap sm:justify-center lg:flex-col lg:border-r lg:border-neutral-200">
         {categories.map((item) => (
           <Link key={item.name} to={`/products/${item.name}`}>
-            <li className="rounded-sm py-2.5 hover:bg-neutral-100">
+            <li className="rounded-sm py-2.5 hover:bg-neutral-100 sm:px-5">
               {item.name}
             </li>
           </Link>
