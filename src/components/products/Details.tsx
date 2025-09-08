4d90e861f56c584238ade5173f765f2dc4b5195f
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-08 03:31:06 +0300

    improve style for phone

--- src/components/products/Details.tsx
+++ src/components/products/Details.tsx
@@ -4,9 +4,11 @@ import type { ProductTypes } from "@/types/products";
 
 function Details({ data }: { data: ProductTypes }) {
   return (
-    <section>
-      <ProductImages data={data} />
-      <ProductInfo data={data} />
+    <section className="py-8 md:py-16">
+      <div className="flex flex-col gap-5 md:flex-row">
+        <ProductImages data={data} />
+        <ProductInfo data={data} />
+      </div>
     </section>
   );
 }
