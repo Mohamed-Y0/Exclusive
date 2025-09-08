4f7c9c5d1dc23c02591b1c2adc502ad24ecd391c
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-08 03:31:38 +0300

    finish: Product images

--- src/components/products/ProductImages.tsx
+++ src/components/products/ProductImages.tsx
@@ -1,7 +1,28 @@
 import type { ProductTypes } from "@/types/products";
 
 function ProductImages({ data }: { data: ProductTypes }) {
-  return <div></div>;
+  return (
+    <div className="flex items-center gap-5">
+      <div className="flex flex-col items-center gap-2.5">
+        {data.images.length > 1 &&
+          data.images.map((img) => (
+            <img
+              key={img}
+              className="w-60 rounded-lg bg-neutral-100"
+              src={img}
+              alt={img}
+            />
+          ))}
+      </div>
+      <div>
+        <img
+          className="rounded-lg bg-neutral-100"
+          src={data.images[0]}
+          alt={data.thumbnail}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ProductImages;
