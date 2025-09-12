e588cd4c7a592444f97a1196d988a8dfa7411b07
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-13 01:45:24 +0300

    add the new hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import BestSelling from "@/components/home/BestSelling";
 import ExploreProducts from "@/components/home/ExploreProducts";
+import FlashSales from "@/components/home/FlashSales";
 import HeroSection from "@/components/home/HeroSection";
 import NewArrival from "@/components/home/NewArrival";
 import StoreBenefits from "@/components/home/StoreBenefits";
@@ -8,6 +9,7 @@ function Home() {
   return (
     <div className={`container m-auto px-5 2xl:p-0`}>
       <HeroSection />
+      <FlashSales />
       <BestSelling />
       <ExploreProducts />
       <NewArrival />
