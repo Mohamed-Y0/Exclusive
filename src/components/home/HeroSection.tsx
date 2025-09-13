5f3d8636708cc4eea2f96a5e12624bd3d088c4ab
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-13 03:06:33 +0300

    improve styles

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -2,9 +2,9 @@ import image from "@/assets/hero-img.webp";
 
 function HeroSection() {
   return (
-    <section className="flex flex-col items-center gap-10 border-b-3 border-neutral-200 py-8 md:flex-row md:gap-20 lg:py-16">
+    <section className="flex flex-col items-center gap-10 border-b-3 border-neutral-200 py-8 md:flex-row md:gap-16 lg:py-20">
       <div className="flex flex-col gap-8">
-        <h1 className="text-5xl font-semibold tracking-wide">
+        <h1 className="text-2xl font-semibold tracking-wide md:text-5xl">
           <span className="text-red-500">Exclusive</span> - Your Online
           Marketplace
         </h1>
