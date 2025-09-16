9ae75a3245a8ee082bfdc41e1259c697f8bd717a
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-17 01:31:22 +0300

    create CartIcon to avoid un-nesscary render in the header

---
+++ src/components/layout/Header/CartIcon.tsx
@@ -0,0 +1,28 @@
+import { IoCartOutline } from "react-icons/io5";
+import { useAppSelector } from "@/store/hooks";
+import { Link } from "react-router-dom";
+import React from "react";
+
+function CartIcon() {
+  const cartItemsLength = useAppSelector(
+    (state) => state.cart.cartItems.length,
+  );
+
+  return (
+    <Link
+      to="/cart"
+      aria-label="Go To Your Cart"
+      role="link"
+      className="relative"
+    >
+      <IoCartOutline className="relative z-50 size-8 lg:size-8" />
+      {cartItemsLength > 0 && (
+        <span className="absolute -top-1/2 -right-2/3 rounded-full bg-red-500 px-2.5 py-0.5 text-neutral-100">
+          {cartItemsLength}
+        </span>
+      )}
+    </Link>
+  );
+}
+
+export default React.memo(CartIcon);
--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,17 +1,13 @@
+import CartIcon from "@/components/layout/Header/CartIcon";
 import HeaderNav from "@/components/layout/Header/HeaderNav";
 import HeaderPhone from "@/components/layout/Header/HeaderPhone";
 import Logo from "@/components/ui/Logo";
-import { useAppSelector } from "@/store/hooks";
 import { useState } from "react";
 import { IoMdMenu } from "react-icons/io";
-import { IoCartOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 function Header() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const cartItemsLength = useAppSelector(
-    (state) => state.cart.cartItems.length,
-  );
 
   function handleMenuToggle(): void {
     setIsOpen((isOpen) => !isOpen);
@@ -42,19 +38,7 @@ function Header() {
             {/* <Link to="" aria-label="Go To Your Wishlist" role="link">
               <IoMdHeartEmpty className="size-6 lg:size-8" />
             </Link> */}
-            <Link
-              to="/cart"
-              aria-label="Go To Your Cart"
-              role="link"
-              className="relative"
-            >
-              <IoCartOutline className="relative z-50 size-8 lg:size-8" />
-              {cartItemsLength > 0 && (
-                <span className="absolute -top-1/2 -right-2/3 rounded-full bg-red-500 px-2.5 py-0.5 text-neutral-100">
-                  {cartItemsLength}
-                </span>
-              )}
-            </Link>
+            <CartIcon />
           </div>
         </div>
       </div>
