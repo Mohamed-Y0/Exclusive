a49b567a1cb569c1f1e4b9d46088f8e131c568ff
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-26 23:52:45 +0300

    add change my passwor link

--- src/components/profile/LeftMenu.tsx
+++ src/components/profile/LeftMenu.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function LeftMenu() {
   return (
-    <div className="">
+    <div className="capitalize">
       <nav>
         <p className="pb-5">Manage My Account</p>
         <ul className="flex flex-col gap-2 pl-10 font-light text-neutral-500">
@@ -14,6 +14,16 @@ function LeftMenu() {
               to="/profile"
             >
               My Profile
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                `${isActive && "text-red-500"} duration-200 hover:text-red-500`
+              }
+              to="/nigga"
+            >
+              change password
             </NavLink>
           </li>
           <li>
