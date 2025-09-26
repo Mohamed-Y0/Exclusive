59ffdd7c8d3dc212334c2ddc9c5294a804f0636e
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-26 07:46:23 +0300

    swap lines to fix problem :)

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -6,8 +6,8 @@ function PrivateRoute({ children }: { children: React.ReactNode }) {
   const isAuth = useAppSelector((state) => state.login.isAuthenticatied);
   const role = useAppSelector((state) => state.login.user?.role);
 
-  if (role !== "admin") return <PageNotFound />;
   if (!isAuth) return <Navigate to="/sign-in" replace />;
+  if (role !== "admin") return <PageNotFound />;
 
   return <>{children}</>;
 }
