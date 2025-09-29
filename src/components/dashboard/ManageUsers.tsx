77f133bb2c0c3bb3cd757656ea0191fdaa2be867
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-30 00:45:26 +0300

    initial uesrs page

--- src/components/dashboard/ManageUsers.tsx
+++ src/components/dashboard/ManageUsers.tsx
@@ -1,4 +1,13 @@
+import Error from "@/components/ui/Error";
+import useUsers from "@/hooks/useUsers";
+
 function ManageUsers() {
+  const { data, isLoading, error } = useUsers();
+
+  if (isLoading) return <p>Loading Losers</p>;
+  if (error) return <Error err={error} />;
+
+  console.log(data);
   return <div>Hello Losers</div>;
 }
 
