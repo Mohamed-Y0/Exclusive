54fcafbbae946a331896fed1e0689b71d460fe85
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-29 06:29:06 +0300

    remove z-index

--- src/components/ui/CartIcon.tsx
+++ src/components/ui/CartIcon.tsx
@@ -15,7 +15,7 @@ function CartIcon() {
       role="link"
       className="relative"
     >
-      <IoCartOutline className="relative z-50 size-8" />
+      <IoCartOutline className="relative size-8" />
       {cartItemsLength > 0 && (
         <span className="absolute -top-1/2 -right-2/3 rounded-full bg-red-500 px-2.5 py-0.5 text-neutral-100">
           {cartItemsLength}
