bb658096281ca3be88996e42815ce846d42c7686
Mohamed-A-Yehia <mohamed.a.yehia9@gmail.com>
2025-09-30 04:45:15 +0300

    initial display users

--- src/components/dashboard/ManageUsers.tsx
+++ src/components/dashboard/ManageUsers.tsx
@@ -1,14 +1,21 @@
+import DisplayData from "@/components/dashboard/DisplayData";
+import SearchBar from "@/components/dashboard/SearchBar";
 import Error from "@/components/ui/Error";
 import useUsers from "@/hooks/useUsers";
 
 function ManageUsers() {
-  const { data, isLoading, error } = useUsers();
+  const { data: users, isLoading, error } = useUsers();
 
   if (isLoading) return <p>Loading Losers</p>;
   if (error) return <Error err={error} />;
 
-  console.log(data);
-  return <div>Hello Losers</div>;
+  console.log(users);
+  return (
+    <div className={`container m-auto px-5 sm:px-0`}>
+      <SearchBar />
+      <DisplayData data={users ?? []} />
+    </div>
+  );
 }
 
 export default ManageUsers;
